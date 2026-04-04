0136ab7ea20e62c66f834ada49ee8002a49d051b
Ranelan <raneyclassic@gmail.com>
2026-04-04 21:23:46 +0200

    add method to find comments by developerId in CommentRepository

--- src/main/java/com/MzansiBuilds/repository/CommentRepository.java
+++ src/main/java/com/MzansiBuilds/repository/CommentRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     List<Comment> findByProjectId(Integer projectId);
+    List<Comment> findByDeveloperId(Integer developerId);
 }
