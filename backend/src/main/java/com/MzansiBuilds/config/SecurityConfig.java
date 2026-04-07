ab56e97f074d20508e81932de08e9b100bbc89da
Ranelan <raneyclassic@gmail.com>
2026-04-08 01:51:43 +0200

    implement security filter chain for HTTP requests

--- backend/src/main/java/com/MzansiBuilds/config/SecurityConfig.java
+++ backend/src/main/java/com/MzansiBuilds/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.MzansiBuilds.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,4 +17,26 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/developer/**",
+                                "/api/project/**",
+                                "/api/milestone/**",
+                                "/api/comment/**",
+                                "/api/collaboration-request/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
 }
